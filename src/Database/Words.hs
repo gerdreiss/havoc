1c54cdfb4c4e7cdb56d15bb174c43acfb2dbf0fd
Gerd Reiss <gerdreiss@jscriptive.com>
2019-09-16 19:31:07 +0200

    comments + constants

--- src/Database/Words.hs
+++ src/Database/Words.hs
@@ -30,21 +30,34 @@ instance FromRow Lexi where
 instance FromRow Int where
   fromRow = field
 
+-- | checks whether the given word exists
 exists :: Text -> IO Bool
 exists lexi = queryWithConn "tr-en.db" existsWord
   where
     existsWord conn = executeSelect conn >>= \res -> return $ L.null res
-    executeSelect conn = query conn "SELECT 1 FROM words WHERE word = ?" (Only lexi) :: IO [Int]
+    executeSelect conn = query conn select1ByWordQuery (Only lexi) :: IO [Int]
 
+-- | find the database entry for the given word
 findWord :: Text -> IO (Maybe Lexi)
 findWord lexi = queryWithConn "tr-en.db" executeQuery
   where
     executeQuery conn  = executeSelect conn >>= maybeHead
-    executeSelect conn = query conn "SELECT * FROM words WHERE word = ?" (Only lexi) :: IO [Lexi]
+    executeSelect conn = query conn selectByWordQuery (Only lexi) :: IO [Lexi]
     maybeHead []       = return Nothing
     maybeHead (x:_)    = return (Just x)
 
+-- | add a new word to the database
 addWord :: Text -> Text -> IO ()
 addWord lexi annotation = executeWithConn "tr-en.db" executeInsert
   where
-    executeInsert conn = execute conn "INSERT INTO words (word, annotation) VALUES (?,?)" (lexi, annotation)
+    executeInsert conn = execute conn insertWordQuery (lexi, annotation)
+
+-- | query constants
+select1ByWordQuery :: Query
+select1ByWordQuery = "SELECT 1 FROM words WHERE word = ?"
+
+selectByWordQuery :: Query
+selectByWordQuery = "SELECT * FROM words WHERE word = ?"
+
+insertWordQuery :: Query
+insertWordQuery = "INSERT INTO words (word, annotation) VALUES (?,?)"
