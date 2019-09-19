aceef6655626fac6b29bae3a86af0c2a2b32dbf9
Gerd Reiss <gerdreiss@jscriptive.com>
2019-09-19 20:32:49 +0200

    Vocabulary: add + get

--- src/Control/Vocabulary.hs
+++ src/Control/Vocabulary.hs
@@ -1,39 +1,42 @@
 module Control.Vocabulary where
 
+import           Control.Monad
 import           Data.Foldable
-import qualified Data.Text             as T
-import qualified Database.Translations as Txs
+import           Data.Maybe
+import qualified Data.Text             as Tx
+import qualified Database.Translations as Ts
 import qualified Database.Words        as W
 
 data Lexi =
   Lexi
-    { word         :: T.Text
-    , translations :: [T.Text]
-    , annotation   :: T.Text
+    { word         :: Tx.Text
+    , translations :: [Tx.Text]
+    , annotation   :: Tx.Text
     }
 
 -- checks whether the given word exists in the vocabulary
-exists :: T.Text -> IO Bool
+exists :: Tx.Text -> IO Bool
 exists = W.exists
 
 -- attempts to retrieve the given word from the vocabulary incl. translations
-get :: T.Text -> IO (Maybe Lexi)
+get :: Tx.Text -> IO (Maybe Lexi)
 get w = do
-  maybePW <- W.findWord w
-  txs <- Txs.findTranslationsForWord w
-  return $ fmap (translations txs) maybePW
+  maybeLexi <- W.findWord w
+  guard (isJust maybeLexi)
+  ts <- Ts.findTranslationsForWord w
+  return $ fmap (withTranslations ts) maybeLexi
   where
-    translations txs pw = Lexi { word = W.value pw
-                               , translations = map Txs.translation txs
-                               , annotation = W.annotation pw
-                               }
+    withTranslations txs pw =
+      Lexi { word = W.value pw
+           , translations = map Ts.translation txs
+           , annotation = W.annotation pw
+           }
 
 -- attempts to add the given Lexi to the vocabulary
 add :: Lexi -> IO ()
-add w = do
-  W.addWord (word w) (annotation w)
-  traverse_ (Txs.addTranslationForWord (word w)) (translations w)
+add w = W.addWord (word w) (annotation w) >>
+        traverse_ (Ts.addTranslationForWord (word w)) (translations w)
 
 -- attempts to update the vocabulary
 update :: Lexi -> IO Bool
-update _ = return False
+update _ = pure False
